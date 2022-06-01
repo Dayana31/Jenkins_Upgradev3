b1e428b6113e5582828685443f8d7137bda23e75
dayana-alarcon <dayana.alarcon@ibm.com>
2022-06-01 16:28:47 -0500

    Test changes

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -40,4 +40,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+  @Test
+  public void greetShouldIncludeGreetingMessage2() {
+    String someone = "World, thanks";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
